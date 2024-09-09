01dd4fa819fd15bcbf60f86ec014f77eec144fc5
Arturs Rijnieks <128680866+Cenone22@users.noreply.github.com>
2024-09-09 23:37:59 +0200

    UserInMemoryRepository

---
+++ Server/InMemoryRepositories/UserInMemoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Entities;
+using Interfaces;
+
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private readonly List<User> users = new();
+
+    public Task<User> AddAsync(User user)
+    {
+        // Generating a new ID for the user
+        user.UserId = users.Count > 0 ? users.Max(u => u.UserId) + 1 : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+    }
+    // Not sure if Update and Delete would be necessary, but in case User data wants to be updated or User to be deleted
+    public Task UpdateAsync(User user)
+    {
+        var existingUser = users.SingleOrDefault(u => u.UserId == user.UserId);
+        if (existingUser == null)
+        {
+            throw new InvalidOperationException($"User ID '{user.UserId}' not found");
+        }
+
+        existingUser.UserName = user.UserName;
+        existingUser.Password = user.Password;
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        var userToRemove = users.SingleOrDefault(u => u.UserId == id);
+        if (userToRemove == null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        var user = users.SingleOrDefault(u => u.UserId == id);
+        if (user == null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+
+        return Task.FromResult(user);
+    }
+
+    //
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
+}
---
+++ Server/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+
+namespace Interfaces;
+
+public interface IUserRepository
+{
+// Takes a user, and returns the created user
+    Task<User> AddAsync(User user);
+    // Takes a user (with ID), and just replaces the existing user
+    Task UpdateAsync(User user);
+    // Removes the user with a given ID
+    Task DeleteAsync(int id);
+    // Returns the user matching the given ID
+    Task<User> GetSingleAsync(int id);
+    // Returns an IQueryable for multiple users
+    IQueryable<User> GetMany();
+}
